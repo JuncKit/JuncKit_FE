858dbecc0e2034bd975f219b91ce8722d9d59c98
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-08-20 04:18:52 +0900

    [Add] 와이어 프레임

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { TestData } from './service/TestService';
 import { useEffect } from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import Map from './components/Map';
+// import Chart from './components/Chart';
 
 function App() {
     useEffect(() => {
@@ -9,10 +10,39 @@ function App() {
             console.log(response);
         });
     }, []);
+    const TopBoxWrapper = styled.div`
+        display: flex;
+        padding-left: 45px;
+        margin-bottom: 25px;
+    `;
+    const TopBox = styled.div`
+        margin-right: 24px;
+        width: 313px;
+        height: 164px;
+        background-color: #d9d9d9;
+        margin-top: 152px;
+    `;
+    const Chart = styled.div`
+        background-color: #d9d9d9;
+        margin-right: 24px;
+        width: 650px;
+        height: 341px;
+    `;
 
     return (
         <>
-            <Map />
+            <TopBoxWrapper>
+                <TopBox />
+                <TopBox />
+                <TopBox />
+                <TopBox />
+            </TopBoxWrapper>
+            <TopBoxWrapper>
+                <Chart />
+                <Map />
+            </TopBoxWrapper>
+
+            {/* <Chart /> */}
             <div className="App">서버 띄우자</div>
         </>
     );
